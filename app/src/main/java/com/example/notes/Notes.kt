29fc894a7b2739904eaf366c178accdcc5ff807d
Mônica Marques <mo.menezes.marques@gmail.com>
2021-08-31 23:14:30 -0300

    Aperfeiçoando a camada do Room Database

--- app/src/main/java/com/example/notes/Notes.kt
+++ app/src/main/java/com/example/notes/Notes.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "notes_table")
 data class Notes (
     @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val noteTitle: String,
     val noteDescription: String
         )
--- app/src/main/java/com/example/notes/NotesDao.kt
+++ app/src/main/java/com/example/notes/NotesDao.kt
@@ -4,12 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import com.example.notes.Notes as Notes
 
 @Dao
 interface NotesDao {
 
     @Query("SELECT * FROM notes_table ORDER BY noteTitle ASC")
-    fun getAlphabetizedWords(): List<Notes>
+    fun getAlphabetizedWords(): Flow<List<Notes>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Notes)
